649281555b4199d8218d732522a064890b83a6aa
scloudmani <manish.sakekar@anyhowinfo.com>
2023-07-19 15:44:29 +0530

    Preserve empty folders

---
+++ app/etc/env.php
@@ -0,0 +1,82 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'f4f6ac2a22f25f64e9eecbf57ecc5108'
+    ],
+    'db' => [
+        'table_prefix' => 'any_',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'anyhow',
+                'username' => 'root',
+                'password' => 'root@123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '0ac_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '0ac_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        '3.7.182.76'
+    ],
+    'install' => [
+        'date' => 'Thu, 08 Jun 2023 07:51:51 +0000'
+    ]
+];
